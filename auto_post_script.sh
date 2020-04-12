f47eb581060ae470a6c410666336ef4a3f41990d
manmadewind <manmadewind@gmail.com>
2020-04-12 10:27:36 +0800

    source backup ${now_date}

--- auto_post_script.sh
+++ auto_post_script.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+now_date=`date +%Y%m%d`
+
 git checkout source;
 git add .;
-git commit -a -m 'source backup `date +%Y%m%d`';
+git commit -a -m 'source backup ${now_date}';
 git push origin source;
 hexo g;
 hexo d;
+
+echo "Done ${now_date}"
